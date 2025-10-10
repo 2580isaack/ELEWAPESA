161766c32c53304b6c2756e7611504e749e39cad
2580isaack <125550217+2580isaack@users.noreply.github.com>
2025-10-10 13:25:47 +0300

    Update elewa.py

--- elewa.py
+++ elewa.py
@@ -29,7 +29,6 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import stats
 import bcrypt
 # Machine Learning / Stats
