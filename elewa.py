a3c27f0617fc0fafcc7393eafd08d0c5324bb7bd
2580isaack <125550217+2580isaack@users.noreply.github.com>
2025-10-10 13:27:09 +0300

    Update elewa.py

--- elewa.py
+++ elewa.py
@@ -30,15 +30,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # Machine Learning / Stats
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.svm import SVR
-from sklearn.metrics import mean_squared_error, r2_score
-# Time series analysis
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.arima.model import ARIMA
+
 
 def init_db():
     conn = sqlite3.connect("users.db")
