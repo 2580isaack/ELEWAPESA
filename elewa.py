177196c3e9f6b2ed1aa8e556c603ce3fbf176d53
2580isaack <125550217+2580isaack@users.noreply.github.com>
2025-10-10 13:26:20 +0300

    Update elewa.py

--- elewa.py
+++ elewa.py
@@ -29,7 +29,6 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scipy import stats
 import bcrypt
 # Machine Learning / Stats
 from sklearn.preprocessing import LabelEncoder, StandardScaler
