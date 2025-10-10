269dbd8994fa3764b72fda84da56b816e3c9609f
2580isaack <125550217+2580isaack@users.noreply.github.com>
2025-10-10 13:08:07 +0300

    Update elewa.py

--- elewa.py
+++ elewa.py
@@ -26,6 +26,8 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+print(matplotlib.__version__)
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
