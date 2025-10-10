f518f477a9ab30c3fce7b213574b3552abbf1eea
2580isaack <125550217+2580isaack@users.noreply.github.com>
2025-10-10 14:22:46 +0300

    Update elewa.py

--- elewa.py
+++ elewa.py
@@ -31,7 +31,6 @@ from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.cluster import KMeans
-from pyngrok import ngrok
 import bcrypt
 import io
 from scipy import stats
